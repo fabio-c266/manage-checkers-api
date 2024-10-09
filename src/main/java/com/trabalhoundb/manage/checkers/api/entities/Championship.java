b2f1f0b192afcd32f54034682bcdd627b17fe60b
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-09 19:35:43 -0300

    feat: initial class model

---
+++ src/main/java/com/trabalhoundb/manage/checkers/api/entities/Championship.java
@@ -0,0 +1,29 @@
+package com.trabalhoundb.manage.checkers.api.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "championship")
+@NoArgsConstructor
+@Getter
+public class Championship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private int maxPlayers;
+    private int minPoints;
+    private Administrator createdBy;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+}
