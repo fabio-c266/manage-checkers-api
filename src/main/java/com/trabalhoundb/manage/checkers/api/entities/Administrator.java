1a59e8c9fd17317d7535e0548959686104677813
fabio-c266 <fabiocaldas266@gmail.com>
2024-10-09 19:35:38 -0300

    feat: initial class model

---
+++ src/main/java/com/trabalhoundb/manage/checkers/api/entities/Administrator.java
@@ -0,0 +1,17 @@
+package com.trabalhoundb.manage.checkers.api.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "administrators")
+public class Administrator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String passwordHashed;
+    private Date createdAt;
+}
