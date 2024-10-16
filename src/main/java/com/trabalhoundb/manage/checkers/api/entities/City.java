91bf0ccb6dec5691930c81ddc60538ddb658018d
Joao Santos <joaogabriel.silvasantos10@gmail.com>
2024-10-16 12:55:10 -0300

    relacionamento OneToMany feito entre cidade e estado

--- src/main/java/com/trabalhoundb/manage/checkers/api/entities/City.java
+++ src/main/java/com/trabalhoundb/manage/checkers/api/entities/City.java
@@ -1,6 +1,17 @@
 package com.trabalhoundb.manage.checkers.api.entities;
 
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +26,9 @@ public class City {
 
     @Column(name = "name", unique = true, nullable = false, length = 40)
     private String name;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "city")
+    private List<Address> addresses;
 
     @ManyToOne
     @JoinColumn(name = "fk_state_id")
--- src/main/java/com/trabalhoundb/manage/checkers/api/entities/State.java
+++ src/main/java/com/trabalhoundb/manage/checkers/api/entities/State.java
@@ -1,6 +1,15 @@
 package com.trabalhoundb.manage.checkers.api.entities;
 
-import jakarta.persistence.*;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,4 +27,7 @@ public class State {
 
     @Column(name = "name", unique = true, nullable = false, length = 20)
     private String name;
+    
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "state")
+    private List<City> cities;
 }
