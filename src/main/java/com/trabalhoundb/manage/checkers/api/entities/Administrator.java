7c87b16d045455f731982ce149bb97bde65e0b62
fabio-c266 <fabiocaldas266@gmail.com>
2024-11-05 19:07:18 -0300

    feat: administrator entity

--- src/main/java/com/trabalhoundb/manage/checkers/api/entities/Administrator.java
+++ src/main/java/com/trabalhoundb/manage/checkers/api/entities/Administrator.java
@@ -1,17 +1,70 @@
-//package com.trabalhoundb.manage.checkers.api.entities;
+package com.trabalhoundb.manage.checkers.api.entities;
 
-//import jakarta.persistence.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-//import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
-//@Entity
-//@Table(name = "administrators")
-//public class Administrator {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//    private String name;
-//    private String email;
-//    private String passwordHashed;
-//    private Date createdAt;
-//}
+@Entity
+@Table(name = "administrators")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Administrator implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @Column(name = "password_hashed")
+    private String passwordHashed;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHashed;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
